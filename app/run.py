cb39f89dea808124f956ed0e650635e674bc11e6
miuguelt <miuguelt@gmail.com>
2024-09-24 17:33:20 -0500

    Cambio run

--- app/run.py
+++ app/run.py
@@ -7,4 +7,4 @@ def hello_world():
     return 'Hola Mundo!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
--- app/run1.py
+++ app/run1.py
@@ -1,9 +1,9 @@
 from app import create_app, db
-
+import os
 app = create_app()
 
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
